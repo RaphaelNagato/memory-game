171cd2c07d56d957946aa48b518f89896ce7543e
RaphaelNagato <Raphaelnagato19@gmail.com>
2018-11-19 19:51:15 +0100

    Update Memory Game

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 
 * [Instructions](#instructions)
 * [Contributing](#contributing)
-
+* [Game Instructions](#game instructions)
+* [Dependencies] (#dependencies)
+* [Summary] (#summary)
 ## Instructions
 
 The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
@@ -18,3 +20,17 @@ For specific, detailed instructions, look at the project instructions in the [Ud
 This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
 
 For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+
+## Game Instructions
+* click on the cards to turn over and if they match,they stay open.
+* you can only open two cards at once .
+* you win the game when all the cards have been matched.
+* your game statistics are shown on the top of the board.
+
+## Dependencies
+* Michael Crawford blog influenced some of the functions needed in the game but was not COPIED.
+* The shuffling function is credited to stack overflow.
+
+## Summary
+* the html file is included in `index.html`,the css file in `css/app.css`abd the app's functionality is included in `js/app.js`.
+* Going through the code in the file will show how the game was created.
--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
  * Create a list that holds all of your cards
  */
 
-
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -37,40 +36,237 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-
+/* Variables needed for multiple functions displayed in the global scope */
 const cards = document.getElementsByClassName("card");
 let cardIsOpen = false;
+let BoardIsLocked = false;
 let firstCard, secondCard;
 let openedCards = [];
+let moves = 0;
+let second = 0,
+    minute = 0,
+    hour = 0;
+let time = document.querySelector(".timer");
+let clock;
+let stars = Array.from(document.querySelectorAll(".stars li"));
+let countOfStars;
+let updateMoves = document.querySelector(".moves");
+let matchedPairs = 0;
 
+shuffleCards(); // the function 'shuffleCards' is fired
+
+startClock(); // the function 'startClock' is fired
+
+// An event Listener is added to the cards so that they can open when clicked
+for (let card of cards) {
+    card.addEventListener("click", openCard);
+}
+
+// Event listener added to the replay button 
+let replay = document.querySelector(".replay")
+replay.addEventListener("click", function () {
+    resetGame();
+});
+
+//Event listener added to the cancel button
+let cancel = document.querySelector(".cancel")
+cancel.addEventListener("click", cancelEvent)
+
+//Event listener added to the restart button
+document.querySelector(".restart").addEventListener("click", function () {
+    showGameStats();
+    toggleModalClass();
+    stopClock();
+});
+
+
+/* Turns the list of cards to an array so they can be looped over and shuffled*/
+function shuffleCards() {
+    let CardsToshuffle = Array.from(cards)
+    let ShuffledCards = shuffle(CardsToshuffle);
+    for (card of ShuffledCards) {
+        document.querySelector(".deck").appendChild(card);
+    }
+
+}
+
+
+/* this fires when a card is clicked */
 function openCard() {
+    if (BoardIsLocked) return;
+    if (this === firstCard) return;
     this.classList.add("open");
 
     if (cardIsOpen === false) {
         cardIsOpen = true;
         firstCard = this;
     } else {
-        cardIsOpen = false;
         secondCard = this;
 
         matchCards();
+
+        countMoves();
+
+        checkMoves();
     }
 
 }
 
-
+/* The function to check for similar cards */
 function matchCards() {
     if (firstCard.innerHTML === secondCard.innerHTML) {
-        firstCard.removeEventListener("click", openCard);
-        secondCard.removeEventListener("click", openCard);
+        cardsDoMatch();
     } else {
-        setTimeout(function () {
-            firstCard.classList.remove("open");
-            secondCard.classList.remove("open");
-        }, 1500);
+        cardsDoNotMatch();
     }
 }
 
-for (let card of cards) {
-    card.addEventListener("click", openCard);
+
+/* This fires when cards do match */
+function cardsDoMatch() {
+    firstCard.removeEventListener("click", openCard);
+    secondCard.removeEventListener("click", openCard);
+    matchedPairs++;
+    if (matchedPairs === 8) {
+        setTimeout(gameOver, 1000);
+    };
+    boardReset();
+}
+
+/* This fires when two cards do not match */
+function cardsDoNotMatch() {
+    BoardIsLocked = true;
+    setTimeout(function () {
+        firstCard.classList.remove("open");
+        secondCard.classList.remove("open");
+
+        boardReset();
+    }, 1000);
+}
+
+/* Resets the Board to avoid the logic of the game being scrambled */
+function boardReset() {
+    [cardIsOpen, BoardIsLocked] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+
+/* counts the number of moves the player played and displays it  */
+function countMoves() {
+    moves += 1;
+    updateMoves.innerHTML = moves;
+}
+
+
+/* The code for the timer */
+function startClock() {
+    clock = setInterval(function () {
+        time.textContent = (`${hour} : ${minute} : ${second}`);
+        second++;
+        if (second == 60) {
+            minute++;
+            second = 0;
+        }
+        if (minute == 60) {
+            hour++;
+            minute = 0;
+        }
+    }, 1000);
+}
+
+function stopClock() {
+    clearInterval(clock);
+}
+
+
+/* This function checks the number of moves so as to hide the stars*/
+function checkMoves() {
+    if (moves === 10 || moves === 24 || moves === 40) {
+        removeStar();
+    }
+}
+
+
+/* This function hides the star */
+function removeStar() {
+    for (star of stars) {
+        if (star.style.display !== "none") {
+            star.style.display = "none";
+            break;
+        }
+    }
+}
+
+
+/* This function toggles the modal view between 'hidden' or 'view' */
+function toggleModalClass() {
+    let modal = document.querySelector(".modal");
+    modal.classList.toggle("hidden");
+}
+
+
+/* This appends the value of the game stats to the modal */
+function showGameStats() {
+    let timeStats = document.querySelector(".game-time");
+    let moveStats = document.querySelector(".game-moves");
+    let starStats = document.querySelector(".game-stars");
+    let stars = starCount();
+
+    timeStats.innerHTML = `Time: ${time.textContent}`;
+    moveStats.innerHTML = `Moves: ${moves}`;
+    starStats.innerHTML = `Stars: ${stars}`;
+}
+
+/*  The function checks for the number of visible stars to be shown in the game stats */
+function starCount() {
+    countOfStars = 0;
+    for (star of stars) {
+        if (star.style.display !== 'none') {
+            countOfStars++;
+        }
+    }
+    return countOfStars;
+}
+
+
+/* This function is used to either enable or disable the cancel button in the modal, it is disabled when the game is over but 
+enabled in other cases */
+function cancelEvent() {
+    toggleModalClass();
+    startClock();
+};
+
+
+/* Resets all the card to it's default state*/
+function resetCards() {
+    for (let card of cards) {
+        card.classList.remove("open")
+    }
+}
+
+
+/* Resets the game to be played again */
+function resetGame() {
+    stopClock();
+    toggleModalClass();
+    [second, minute, hour] = [0, 0, 0];
+    moves = 0;
+    updateMoves.innerHTML = 0;
+    for (star of stars) {
+        star.style.display = "inline";
+    };
+    shuffleCards();
+    resetCards();
+    startClock();
+}
+
+
+/* this fires when all the cards have been matched */
+function gameOver() {
+    showGameStats();
+    toggleModalClass();
+    stopClock();
+    alert("You have won");
+    cancel.removeEventListener("click", cancelEvent);
+
 }
